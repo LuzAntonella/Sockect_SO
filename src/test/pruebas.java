7419110d8fe1f9f0886d5a9e6c27d07dc07df24c
Light <cabrejosseverinoluzantonella@gmail.com>
2021-03-08 18:03:26 -0500

    agregar test

---
+++ src/test/pruebas.java
@@ -0,0 +1,14 @@
+
+package test;
+
+import java.net.Socket;
+import proyecto.Usuario;
+
+public class pruebas {
+    private int puerto;
+    private String url;
+    private Socket socket;
+    private boolean estadoConexion;
+    Usuario ventana;
+    private String nombre;
+}
